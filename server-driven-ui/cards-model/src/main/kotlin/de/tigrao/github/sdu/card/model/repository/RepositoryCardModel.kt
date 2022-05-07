09626f1b05a77b72bed1fdf82a1c0e480b359bae
Gabriel Costa <gabrielcosta08@gmail.com>
2022-05-07 09:26:39 -0300

    Make card models be a interface

--- server-driven-ui/cards-model/src/main/kotlin/de/tigrao/github/sdu/card/model/repository/RepositoryCardModel.kt
+++ server-driven-ui/cards-model/src/main/kotlin/de/tigrao/github/sdu/card/model/repository/RepositoryCardModel.kt
@@ -1,5 +1,7 @@
 package de.tigrao.github.sdu.card.model.repository
 
+import de.tigrao.github.sdu.card.model.CardModel
+
 data class RepositoryCardModel(
     val image: String,
     val name: String,
@@ -7,9 +9,9 @@ data class RepositoryCardModel(
     val description: String,
     val stars: String,
     val language: LanguageModel,
-)
+): CardModel
 
 data class LanguageModel(
     val language: String,
     val color: String,
-)
+): CardModel
