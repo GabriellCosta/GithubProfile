cd937152cea05351937af191dbca3dd4ffc82085
Gabriel Costa <gabrielcosta08@gmail.com>
2022-05-09 06:54:26 -0300

    update repo card to show cor color

--- server-driven-ui/cards-ui/src/main/kotlin/dev/tigrao/github/sdu/card/ui/repository/RepositoryCardItem.kt
+++ server-driven-ui/cards-ui/src/main/kotlin/dev/tigrao/github/sdu/card/ui/repository/RepositoryCardItem.kt
@@ -1,7 +1,12 @@
 package dev.tigrao.github.sdu.card.ui.repository
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
 import android.view.View
 import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.google.android.material.card.MaterialCardView
 import com.xwray.groupie.viewbinding.BindableItem
@@ -14,7 +19,6 @@ import dev.tigrao.github.sdu.card.ui.support.loadImage
 internal class RepositoryCardItem(
     private val card: RepositoryCardModel,
 ) : BindableItem<CardsRepositoryBinding>() {
-
     override fun bind(viewBinding: CardsRepositoryBinding, position: Int) {
         with(viewBinding) {
             cardImage.loadImage(card.image)
@@ -27,10 +31,19 @@ internal class RepositoryCardItem(
 
             this.txtLanguage.text = card.language?.language
 
+            val dot = ContextCompat.getDrawable(root.context, R.drawable.cards_language_dot)
+            card.language?.let {
+                (dot as? GradientDrawable)?.color =
+                    ColorStateList.valueOf(Color.parseColor(it.color))
+            }
 
-            when(card.cardSize) {
-                CardSize.FULL -> this.root.layoutParams.width = FrameLayout.LayoutParams.MATCH_PARENT
-                CardSize.MINI -> this.root.layoutParams.width = this.root.resources.getDimensionPixelSize(R.dimen.cards_repository_size_mini)
+            txtLanguage.setCompoundDrawablesWithIntrinsicBounds(dot, null, null, null)
+
+            when (card.cardSize) {
+                CardSize.FULL -> this.root.layoutParams.width =
+                    FrameLayout.LayoutParams.MATCH_PARENT
+                CardSize.MINI -> this.root.layoutParams.width =
+                    this.root.resources.getDimensionPixelSize(R.dimen.cards_repository_size_mini)
             }
         }
     }
