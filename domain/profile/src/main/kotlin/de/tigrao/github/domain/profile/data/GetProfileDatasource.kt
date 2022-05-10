6e250a4012cd2f2ff2d8b6b2d0ac58cbbd814f3a
Gabriel Costa <gabrielcosta08@gmail.com>
2022-05-09 21:43:31 -0300

    Create class to expire cache if needed

--- domain/profile/src/main/kotlin/de/tigrao/github/domain/profile/data/GetProfileDatasource.kt
+++ domain/profile/src/main/kotlin/de/tigrao/github/domain/profile/data/GetProfileDatasource.kt
@@ -2,25 +2,20 @@ package de.tigrao.github.domain.profile.data
 
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.cache.normalized.FetchPolicy
-import com.apollographql.apollo3.cache.normalized.apolloStore
 import com.apollographql.apollo3.cache.normalized.fetchPolicy
-import de.tigrao.github.domain.profile.domain.ShouldExpireCache
 import dev.tigrao.github.UserProfileQuery
 import javax.inject.Inject
 
 internal class GetProfileDatasource @Inject constructor(
     private val apolloClient: ApolloClient,
-    private val shouldExpireCache: ShouldExpireCache,
-    private val expireCacheDatasource: ExpireCacheDatasource,
+    private val expireCacheIfNeeded: ExpireCacheIfNeeded
 ) {
 
     suspend fun fetchProfileData(
         force: Boolean
     ): UserProfileQuery.Data {
-        if (force || shouldExpireCache()) {
-            apolloClient.apolloStore.clearAll()
-            expireCacheDatasource.deleteAndSaveTime()
-        }
+
+        expireCacheIfNeeded.expireIfNeeded(force)
 
         return apolloClient
             .query(UserProfileQuery())
