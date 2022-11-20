ef6d53174c528976ea3698101836b3c003150cce
Gabriel Goto <gabrielcosta08@gmail.com>
2022-11-20 17:21:59 +0000

    Add loggin interceptor for debug

--- infra/network/impl/src/main/java/dev/tigrao/github/infra/network/OkhttpClientFactory.kt
+++ infra/network/impl/src/main/java/dev/tigrao/github/infra/network/OkhttpClientFactory.kt
@@ -9,14 +9,18 @@ private const val CONNECTION_TIMEOUT = 1L
 private const val READ_TIMEOUT = 30L
 private const val WRITE_TIMEOUT = 15L
 
-class OkhttpClientFactory @Inject constructor(
-    private val interceptor: Interceptor
+internal class OkhttpClientFactory @Inject constructor(
+    private val interceptor: Set<@JvmSuppressWildcards Interceptor>,
 ) {
 
     fun createNewInstance(): OkHttpClient =
         OkHttpClient.Builder().connectTimeout(CONNECTION_TIMEOUT, TimeUnit.MINUTES)
             .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
             .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
-            .addInterceptor(interceptor)
+            .apply {
+                interceptor.forEach {
+                    addInterceptor(it)
+                }
+            }
             .build()
 }
--- infra/network/impl/src/main/java/dev/tigrao/github/infra/network/di/NetworkModule.kt
+++ infra/network/impl/src/main/java/dev/tigrao/github/infra/network/di/NetworkModule.kt
@@ -1,14 +1,18 @@
 package dev.tigrao.github.infra.network.di
 
 import android.app.Application
+import com.apollographql.apollo3.cache.normalized.sql.BuildConfig
 import com.apollographql.apollo3.cache.normalized.sql.SqlNormalizedCacheFactory
 import dagger.Module
 import dagger.Provides
+import dagger.multibindings.ElementsIntoSet
+import dagger.multibindings.IntoSet
 import dev.tigrao.github.infra.network.NetworkBuilder
 import dev.tigrao.github.infra.network.NetworkService
 import dev.tigrao.github.infra.network.OkhttpClientFactory
 import dev.tigrao.github.infra.network.interceptor.AuthorizationInterceptor
 import okhttp3.Interceptor
+import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Singleton
 
 @Module
@@ -17,13 +21,22 @@ object NetworkModule {
     private const val CACHE_NAME = "apollo.db"
 
     @Provides
-    @Singleton
+    @IntoSet
     fun provideAuthInterceptor(networkBuilder: NetworkBuilder): Interceptor =
         AuthorizationInterceptor(networkBuilder)
 
     @Provides
+    @ElementsIntoSet
+    fun providesLoggerInterceptor(): Set<Interceptor> =
+        if (BuildConfig.DEBUG) {
+            setOf(HttpLoggingInterceptor())
+        } else {
+            emptySet<Interceptor>()
+        }
+
+    @Provides
     @Singleton
-    fun provideApollo(
+    internal fun provideApollo(
         networkBuilder: NetworkBuilder,
         okhttpClientFactory: OkhttpClientFactory,
         normalizedCacheFactory: SqlNormalizedCacheFactory,
